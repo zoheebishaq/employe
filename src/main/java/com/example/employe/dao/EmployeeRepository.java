05f34361740e7c836797eacd3c65bc3c6d1a75dc
zoheebishaq <zoheeb.ishaque@gmail.com>
2021-05-18 18:56:38 +0200

    Initial commit

---
+++ src/main/java/com/example/employe/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.example.employe.dao;
+
+import com.example.employe.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+
+}
---
+++ src/main/java/com/example/employe/service/EmployeeServiceImpl.java
@@ -0,0 +1,55 @@
+package com.example.employe.service;
+
+import com.example.employe.dao.EmployeeRepository;
+import com.example.employe.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+    @Override
+    public void saveEmployee(Employee employee){
+        this.employeeRepository.save(employee);
+    }
+
+    @Override
+    public Employee getEmployeebyId(Long id) {
+        Optional<Employee> optional = employeeRepository.findById(id);
+        Employee employee = null;
+    if(optional.isPresent()){
+        employee = optional.get();
+    }
+    else{
+        throw new RuntimeException("Employee not found for id :: "+id);
+    }
+        return employee;
+    }
+
+    @Override
+    public void deleteEmployeeById(Long id) {
+        this.employeeRepository.deleteById(id);
+    }
+
+    @Override
+    public Page<Employee> findPaginated(int pageNo, int pageSize,String sortField, String sortDirection) {
+        Sort sort = sortDirection.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending() :
+                Sort.by(sortField).descending();
+        Pageable pageable = PageRequest.of(pageNo - 1,pageSize, sort);
+        return this.employeeRepository.findAll(pageable);
+    }
+
+
+}
